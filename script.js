43d313b2343964e3bc8e5c428b6579ab8c4dc307
digusmao <143654123+digusmao@users.noreply.github.com>
2025-06-27 15:47:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const botao = document.getElementById("botao");
+const corpo = document.body;
+
+corpo.addEventListener("mousemove", function (evento) {
+    let mouseX = evento.clientX;
+    let mouseY = evento.clientY;
+
+    let pocicaoBotao = botao.getBoundingClientRect();
+
+    const distanciaLimite = 100;
+
+    let centroBotaoX = posicaoBotao.left + posicaoBotao.width / 2;
+    let centroBotaoY = posicaoBotao.top + posicaoBotao.heigt / 2;
+
+    let distanciaX = mouseX - centroBotaoX;
+    let distanciaY = mouseY - centroBotaoY;
+
+    if (Math.abs(distanciaX) < distanciaLimite &&
+        Math.abs(distanciaY) < distanciaLimite
+
+    ) {
+
+        let moverX = (distanciaX > 0 ? -1 : 1) * distanciaLimite;
+        let moverY = (distanciaY > 0 ? -1 : 1) * distanciaLimite;
+
+        let novaPosicaoX = Math.min(window.innerWidth - posicaoBotao.width, Math.max(0, botao.offsetLeft + moverX));
+
+        let novaPosicaoY = Math.min(window.innerHeight - posicaoBotao.heigt, Math.max(0, botao.offsetTop + moverY));
+        botao.style.position = "absolute";
+        botao.style.left = novaPosicaoX + "px";
+        botao.style.left = novaPosicaoY + "px";
+
+
+    }
+
+}); 
